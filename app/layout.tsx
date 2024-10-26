b8b35e6e8c24065c221dfe857749bdcc26a69656
NZ <ns@zav.me>
2024-10-27 01:16:27 +0300

    Updated manifest, updated favicon. Added a 512x512 favicon.

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import { Inter } from 'next/font/google';
 import './globals.css';
 import MainNavigation from '@/components/ui/main-navigation';
 import { ReactNode } from 'react';
-import Head from 'next/head';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -13,6 +12,28 @@ export const metadata: Metadata = {
         default: "Natalia's blog",
     },
     description: 'I post about programming and web development.',
+    icons: {
+        icon: [
+            '/favicon.ico',
+            {
+                url: '/icons/favicon-16x16.png',
+                sizes: '16x16',
+                type: 'image/png',
+            },
+            {
+                url: '/icons/favicon-32x32.png',
+                sizes: '32x32',
+                type: 'image/png',
+            },
+            {
+                url: '/icons/android-icon-192x192.png',
+                sizes: '192x192',
+                type: 'image/png',
+            },
+        ],
+        apple: '/icons/apple-touch-icon.png',
+    },
+    manifest: '/manifest.webmanifest',
 };
 
 export default function RootLayout({
@@ -22,61 +43,6 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="ru">
-            <Head>
-                <link rel="icon" href="/favicon.ico" />
-
-                <link
-                    rel="apple-touch-icon"
-                    sizes="180x180"
-                    href="/icons/apple-touch-icon.png"
-                />
-
-                <link
-                    rel="icon"
-                    type="image/png"
-                    sizes="36x36"
-                    href="/icons/android-icon-36x36.png"
-                />
-                <link
-                    rel="icon"
-                    type="image/png"
-                    sizes="48x48"
-                    href="/icons/android-icon-48x48.png"
-                />
-                <link
-                    rel="icon"
-                    type="image/png"
-                    sizes="72x72"
-                    href="/icons/android-icon-72x72.png"
-                />
-                <link
-                    rel="icon"
-                    type="image/png"
-                    sizes="96x96"
-                    href="/icons/android-icon-96x96.png"
-                />
-                <link
-                    rel="icon"
-                    type="image/png"
-                    sizes="192x192"
-                    href="/icons/android-icon-192x192.png"
-                />
-
-                <link
-                    rel="icon"
-                    type="image/png"
-                    sizes="32x32"
-                    href="/icons/favicon-32x32.png"
-                />
-                <link
-                    rel="icon"
-                    type="image/png"
-                    sizes="16x16"
-                    href="/icons/favicon-16x16.png"
-                />
-
-                <link rel="manifest" href="/app/manifest.webmanifest" />
-            </Head>
             <body className={inter.className}>
                 <div className="flex min-h-screen flex-col">
                     <MainNavigation />
