e9989638d0c1bcefa02ddc07e6b98cd9f5c43882
NZ <ns@zav.me>
2025-02-13 15:55:15 +0300

    update CookieConsent style

--- components/ui/CookieConsent.tsx
+++ components/ui/CookieConsent.tsx
@@ -23,14 +23,16 @@ export default function CookieConsent() {
     if (!isVisible) return null;
 
     return (
-        <div className="fixed bottom-4 left-4 right-4 max-w-xl mx-auto bg-primary-contrast dark:bg-gray-900 text-foreground p-4 rounded-2xl shadow-md">
+        <div className="fixed bottom-3 right-4 max-w-lg text-foreground p-4 rounded-2xl shadow-md
+        bg-primary-light dark:bg-gray-900
+        border border-primary-contrast dark:border-dark-strong">
             <p className="text-sm">
                 We use cookies to improve the website&#39;s functionality. By continuing to use this site, you agree to our&nbsp;
                 <Link href="/privacy-policy" className="text-blue-500 dark:text-blue-400 underline">
                     Privacy Policy
                 </Link>.
             </p>
-            <div className="mt-3 flex justify-end gap-2">
+            <div className="flex justify-end">
                 <button onClick={handleAccept} className="button-accent button-sm">
                     Ok
                 </button>
