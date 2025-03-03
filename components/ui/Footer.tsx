c36a7fbad0ff8093ec1932adca1b6a16cd0a361c
NZ <ns@zav.me>
2025-03-03 17:49:17 +0300

    added a link to github in the footer

--- components/ui/Footer.tsx
+++ components/ui/Footer.tsx
@@ -1,8 +1,31 @@
+import Link from 'next/link';
+
 export default function Footer() {
     const currentYear = new Date().getFullYear();
     return (
-        <div className='w-full flex justify-center items-center bg-primary dark:bg-dark p-2'>
-            <p className='text-muted-light text-xs'>© {currentYear} Natalia&apos;s Next blog</p>
+        <div className="flex gap-2 w-full items-center justify-center bg-primary p-3 dark:bg-dark">
+            <p className="text-xs text-muted-light">
+                © {currentYear} Natalia&apos;s Next blog
+            </p>
+            <Link
+                href="https://github.com/ZavNatalia"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="flex items-center gap-2 text-muted-light hover:text-accent dark:hover:text-accent-dark transition"
+            >
+                <svg
+                    className="w-5 h-5"
+                    fill="currentColor"
+                    viewBox="0 0 24 24"
+                    aria-hidden="true"
+                >
+                    <path
+                        fillRule="evenodd"
+                        d="M12 2C6.48 2 2 6.48 2 12c0 4.42 2.87 8.17 6.84 9.49.5.09.66-.22.66-.49v-1.7c-2.78.6-3.37-1.34-3.37-1.34-.45-1.14-1.11-1.45-1.11-1.45-.9-.62.07-.6.07-.6 1 .07 1.52 1.04 1.52 1.04.89 1.52 2.34 1.08 2.91.83.09-.64.35-1.08.63-1.33-2.22-.25-4.56-1.11-4.56-4.95 0-1.09.39-1.98 1.03-2.67-.1-.25-.45-1.27.1-2.64 0 0 .84-.27 2.75 1.02A9.56 9.56 0 0 1 12 6.8c.85 0 1.7.11 2.5.33 1.91-1.29 2.75-1.02 2.75-1.02.55 1.37.2 2.39.1 2.64.64.69 1.03 1.58 1.03 2.67 0 3.85-2.34 4.7-4.57 4.95.36.31.68.92.68 1.86v2.77c0 .27.16.59.67.49A10.005 10.005 0 0 0 22 12c0-5.52-4.48-10-10-10Z"
+                        clipRule="evenodd"
+                    />
+                </svg>
+            </Link>
         </div>
-    )
+    );
 }
