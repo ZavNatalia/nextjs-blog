44259e4a9b75d6f0687d76ace431b58105bf9bfd
NZ <ns@zav.me>
2026-02-23 20:14:41 +0300

    fix: align skeleton components with actual component markup

--- components/ui/posts/post-card/post-card-skeleton.tsx
+++ components/ui/posts/post-card/post-card-skeleton.tsx
@@ -1,11 +1,11 @@
 export function PostCardSkeleton() {
     return (
-        <li className="w-full overflow-hidden rounded-3xl shadow-md">
-            <div className="flex justify-between rounded-t-3xl bg-background-tertiary/50 px-5 py-4 shadow-md lg:px-6 lg:py-5">
+        <li className="h-fit w-full overflow-hidden rounded-2xl shadow-xs">
+            <div className="flex justify-between bg-background-tertiary/50 px-5 py-4 lg:px-6 lg:py-5">
                 <div className="h-7 w-2/5 animate-pulse rounded-lg bg-foreground-muted/20 lg:h-8" />
                 <div className="h-4 w-20 rounded-lg bg-foreground-muted/20" />
             </div>
-            <div className="bg-secondary flex flex-col justify-between gap-4 rounded-b-3xl px-5 pb-5 pt-4 shadow-md lg:px-6">
+            <div className="bg-secondary flex flex-col justify-between gap-4 px-5 pb-5 pt-4 shadow-md lg:px-6">
                 <div className="mt-1 flex flex-col gap-4">
                     <div className="bg-tertiary h-4 w-full rounded-lg" />
                     <div className="bg-tertiary h-4 w-5/6 rounded-lg" />
