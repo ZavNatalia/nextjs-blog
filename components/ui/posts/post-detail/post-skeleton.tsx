644de5a9699add04b9763e2cf18b874da7366df7
NZ <ns@zav.me>
2026-02-23 20:14:41 +0300

    fix: align skeleton components with actual component markup

--- components/ui/posts/post-detail/post-skeleton.tsx
+++ components/ui/posts/post-detail/post-skeleton.tsx
@@ -1,7 +1,7 @@
 export default function PostSkeleton() {
     return (
-        <div className="mt-[40px] w-full max-w-7xl space-y-4 p-3 md:p-4 lg:mt-[44px] lg:p-10">
-            <header className="relative mb-6 flex animate-pulse flex-col gap-3 pb-6 after:absolute after:bottom-0 after:block after:h-2 after:w-full after:bg-accent-700 md:mb-10 md:flex-row md:justify-between md:gap-10 md:pb-8">
+        <div className="mx-auto w-full space-y-4 rounded-3xl p-3 md:p-4 lg:max-w-7xl lg:p-10">
+            <header className="relative mb-6 flex animate-pulse flex-col gap-3 pb-6 after:absolute after:bottom-0 after:block after:h-2 after:w-full after:bg-accent md:mb-10 md:flex-row md:justify-between md:gap-10 md:pb-8">
                 <div className="w-full">
                     <div className="mb-4 h-12 max-w-96 rounded-lg bg-background-tertiary md:h-14"></div>
                     <div className="h-4 max-w-32 rounded-lg bg-background-tertiary md:h-5 md:max-w-44"></div>
