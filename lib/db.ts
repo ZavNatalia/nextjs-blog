8ae3f1e99263fdde6d492926fd9fc773856d54dc
NZ <ns@zav.me>
2026-02-27 00:23:06 +0300

    fix: MongoDB connectionString

--- lib/db.ts
+++ lib/db.ts
@@ -1,7 +1,6 @@
 import { MongoClient } from 'mongodb';
 
-// const connectionString = `mongodb+srv://${process.env.mongodb_username}:${process.env.mongodb_password}@${process.env.mongodb_clustername}.1wiukyn.mongodb.net/${process.env.mongodb_database}?retryWrites=true&w=majority`;
-const connectionString = `mongodb+srv://${process.env.mongodb_username}:${process.env.mongodb_password}@${process.env.mongodb_clustername}.uncch6e.mongodb.net/?appName=${process.env.mongodb_database}`;
+const connectionString = `mongodb+srv://${process.env.mongodb_username}:${process.env.mongodb_password}@${process.env.mongodb_clustername}.uncch6e.mongodb.net/${process.env.mongodb_database}?retryWrites=true&w=majority&appName=${process.env.mongodb_database}`;
 
 if (!connectionString) {
     throw new Error('MONGODB_URI is not defined in the environment variables');
